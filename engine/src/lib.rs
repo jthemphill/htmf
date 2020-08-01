6d64f3c07d3b098c5f882660971dd1356f33a67c
Jeff Hemphill <jhemphill@fb.com>
2020-08-01 13:13:58 -0700

    Update Rust dependencies

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -9,7 +9,7 @@ extern crate serde_derive;
 
 pub const EVEN_ROW_LEN: usize = 7;
 pub const ODD_ROW_LEN: usize = 8;
-pub const NUM_CELLS: usize = (EVEN_ROW_LEN * (NUM_ROWS / 2) + ODD_ROW_LEN * (NUM_ROWS / 2));
+pub const NUM_CELLS: usize = EVEN_ROW_LEN * (NUM_ROWS / 2) + ODD_ROW_LEN * (NUM_ROWS / 2);
 pub const NUM_ROWS: usize = 8;
 pub const NUM_ONE_FISH: usize = 30;
 pub const NUM_TWO_FISH: usize = 20;
