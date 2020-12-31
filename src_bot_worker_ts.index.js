5bde27f29fb32e599406d882402868b2563bf706
Jeff Hemphill <jhemphill@fb.com>
2020-12-31 13:33:53 -0800

    Updates

---
+++ src_bot_worker_ts.index.js
@@ -0,0 +1,335 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/bot.worker.ts":
+/*!***************************!*\
+  !*** ./src/bot.worker.ts ***!
+  \***************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+eval("module.exports = (async () => {\n__webpack_require__.r(__webpack_exports__);\nconst ctx = self;\nconst Bot = (await __webpack_require__.e(/*! import() */ \"src_Bot_ts\").then(__webpack_require__.bind(__webpack_require__, /*! ./Bot */ \"./src/Bot.ts\"))).default;\nconst bot = new Bot(ctx.postMessage);\nctx.onmessage = (event) => {\n    bot.onMessage(event.data);\n};\nbot.init();\n\n\nreturn __webpack_exports__;\n})();\n\n//# sourceURL=webpack://htmf/./src/bot.worker.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			id: moduleId,
+/******/ 			loaded: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = __webpack_module_cache__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/ensure chunk */
+/******/ 	(() => {
+/******/ 		__webpack_require__.f = {};
+/******/ 		// This file contains only the entry chunk.
+/******/ 		// The chunk loading function for additional chunks
+/******/ 		__webpack_require__.e = (chunkId) => {
+/******/ 			return Promise.all(Object.keys(__webpack_require__.f).reduce((promises, key) => {
+/******/ 				__webpack_require__.f[key](chunkId, promises);
+/******/ 				return promises;
+/******/ 			}, []));
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/get javascript chunk filename */
+/******/ 	(() => {
+/******/ 		// This function allow to reference async chunks
+/******/ 		__webpack_require__.u = (chunkId) => {
+/******/ 			// return url for filenames based on template
+/******/ 			return "" + chunkId + ".index.js";
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/harmony module decorator */
+/******/ 	(() => {
+/******/ 		__webpack_require__.hmd = (module) => {
+/******/ 			module = Object.create(module);
+/******/ 			if (!module.children) module.children = [];
+/******/ 			Object.defineProperty(module, 'exports', {
+/******/ 				enumerable: true,
+/******/ 				set: () => {
+/******/ 					throw new Error('ES Modules may not assign module.exports or exports.*, Use ESM export syntax, instead: ' + module.id);
+/******/ 				}
+/******/ 			});
+/******/ 			return module;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/importScripts chunk loading */
+/******/ 	(() => {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded chunks
+/******/ 		// "1" means "already loaded"
+/******/ 		var installedChunks = {
+/******/ 			"src_bot_worker_ts": 1
+/******/ 		};
+/******/ 		
+/******/ 		// importScripts chunk loading
+/******/ 		var chunkLoadingCallback = (data) => {
+/******/ 			var [chunkIds, moreModules, runtime] = data;
+/******/ 			for(var moduleId in moreModules) {
+/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 				}
+/******/ 			}
+/******/ 			if(runtime) runtime(__webpack_require__);
+/******/ 			while(chunkIds.length)
+/******/ 				installedChunks[chunkIds.pop()] = 1;
+/******/ 			parentChunkLoadingFunction(data);
+/******/ 		};
+/******/ 		__webpack_require__.f.i = (chunkId, promises) => {
+/******/ 			// "1" is the signal for "already loaded"
+/******/ 			if(!installedChunks[chunkId]) {
+/******/ 				importScripts("" + __webpack_require__.u(chunkId));
+/******/ 			}
+/******/ 		};
+/******/ 		
+/******/ 		var chunkLoadingGlobal = globalThis["webpackChunkhtmf"] = globalThis["webpackChunkhtmf"] || [];
+/******/ 		var parentChunkLoadingFunction = chunkLoadingGlobal.push.bind(chunkLoadingGlobal);
+/******/ 		chunkLoadingGlobal.push = chunkLoadingCallback;
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/wasm chunk loading */
+/******/ 	(() => {
+/******/ 		// object to store loaded and loading wasm modules
+/******/ 		var installedWasmModules = {};
+/******/ 		
+/******/ 		function promiseResolve() { return Promise.resolve(); }
+/******/ 		
+/******/ 		var wasmImportedFuncCache0;
+/******/ 		var wasmImportedFuncCache1;
+/******/ 		var wasmImportedFuncCache2;
+/******/ 		var wasmImportedFuncCache3;
+/******/ 		var wasmImportedFuncCache4;
+/******/ 		var wasmImportedFuncCache5;
+/******/ 		var wasmImportedFuncCache6;
+/******/ 		var wasmImportedFuncCache7;
+/******/ 		var wasmImportedFuncCache8;
+/******/ 		var wasmImportedFuncCache9;
+/******/ 		var wasmImportedFuncCache10;
+/******/ 		var wasmImportedFuncCache11;
+/******/ 		var wasmImportedFuncCache12;
+/******/ 		var wasmImportedFuncCache13;
+/******/ 		var wasmImportedFuncCache14;
+/******/ 		var wasmImportObjects = {
+/******/ 			"../pkg/htmf_wasm_bg.wasm": function() {
+/******/ 				return {
+/******/ 					"./htmf_wasm_bg.js": {
+/******/ 						"__wbindgen_string_new": function(p0i32,p1i32) {
+/******/ 							if(wasmImportedFuncCache0 === undefined) wasmImportedFuncCache0 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache0["__wbindgen_string_new"](p0i32,p1i32);
+/******/ 						},
+/******/ 						"__wbg_new_59cb74e423758ede": function() {
+/******/ 							if(wasmImportedFuncCache1 === undefined) wasmImportedFuncCache1 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache1["__wbg_new_59cb74e423758ede"]();
+/******/ 						},
+/******/ 						"__wbg_stack_558ba5917b466edd": function(p0i32,p1i32) {
+/******/ 							if(wasmImportedFuncCache2 === undefined) wasmImportedFuncCache2 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache2["__wbg_stack_558ba5917b466edd"](p0i32,p1i32);
+/******/ 						},
+/******/ 						"__wbg_error_4bb6c2a97407129a": function(p0i32,p1i32) {
+/******/ 							if(wasmImportedFuncCache3 === undefined) wasmImportedFuncCache3 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache3["__wbg_error_4bb6c2a97407129a"](p0i32,p1i32);
+/******/ 						},
+/******/ 						"__wbindgen_object_drop_ref": function(p0i32) {
+/******/ 							if(wasmImportedFuncCache4 === undefined) wasmImportedFuncCache4 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache4["__wbindgen_object_drop_ref"](p0i32);
+/******/ 						},
+/******/ 						"__wbg_getRandomValues_11115a852729f4e8": function(p0i32,p1i32,p2i32) {
+/******/ 							if(wasmImportedFuncCache5 === undefined) wasmImportedFuncCache5 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache5["__wbg_getRandomValues_11115a852729f4e8"](p0i32,p1i32,p2i32);
+/******/ 						},
+/******/ 						"__wbg_randomFillSync_a2d002fc3b8e30f7": function(p0i32,p1i32,p2i32) {
+/******/ 							if(wasmImportedFuncCache6 === undefined) wasmImportedFuncCache6 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache6["__wbg_randomFillSync_a2d002fc3b8e30f7"](p0i32,p1i32,p2i32);
+/******/ 						},
+/******/ 						"__wbg_self_a5f0fe5564782787": function() {
+/******/ 							if(wasmImportedFuncCache7 === undefined) wasmImportedFuncCache7 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache7["__wbg_self_a5f0fe5564782787"]();
+/******/ 						},
+/******/ 						"__wbg_static_accessor_MODULE_7f278c5446c126c8": function() {
+/******/ 							if(wasmImportedFuncCache8 === undefined) wasmImportedFuncCache8 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache8["__wbg_static_accessor_MODULE_7f278c5446c126c8"]();
+/******/ 						},
+/******/ 						"__wbg_require_29e58b5f6f133563": function(p0i32,p1i32,p2i32) {
+/******/ 							if(wasmImportedFuncCache9 === undefined) wasmImportedFuncCache9 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache9["__wbg_require_29e58b5f6f133563"](p0i32,p1i32,p2i32);
+/******/ 						},
+/******/ 						"__wbg_crypto_d91429ea1a087f70": function(p0i32) {
+/******/ 							if(wasmImportedFuncCache10 === undefined) wasmImportedFuncCache10 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache10["__wbg_crypto_d91429ea1a087f70"](p0i32);
+/******/ 						},
+/******/ 						"__wbg_msCrypto_c8be2bb4fc7d8cd3": function(p0i32) {
+/******/ 							if(wasmImportedFuncCache11 === undefined) wasmImportedFuncCache11 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache11["__wbg_msCrypto_c8be2bb4fc7d8cd3"](p0i32);
+/******/ 						},
+/******/ 						"__wbindgen_is_undefined": function(p0i32) {
+/******/ 							if(wasmImportedFuncCache12 === undefined) wasmImportedFuncCache12 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache12["__wbindgen_is_undefined"](p0i32);
+/******/ 						},
+/******/ 						"__wbindgen_throw": function(p0i32,p1i32) {
+/******/ 							if(wasmImportedFuncCache13 === undefined) wasmImportedFuncCache13 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache13["__wbindgen_throw"](p0i32,p1i32);
+/******/ 						},
+/******/ 						"__wbindgen_rethrow": function(p0i32) {
+/******/ 							if(wasmImportedFuncCache14 === undefined) wasmImportedFuncCache14 = __webpack_require__.c["../pkg/htmf_wasm_bg.js"].exports;
+/******/ 							return wasmImportedFuncCache14["__wbindgen_rethrow"](p0i32);
+/******/ 						}
+/******/ 					}
+/******/ 				};
+/******/ 			},
+/******/ 		};
+/******/ 		
+/******/ 		var wasmModuleMap = {
+/******/ 			"src_Bot_ts": [
+/******/ 				"../pkg/htmf_wasm_bg.wasm"
+/******/ 			]
+/******/ 		};
+/******/ 		
+/******/ 		// object with all WebAssembly.instance exports
+/******/ 		__webpack_require__.w = {};
+/******/ 		
+/******/ 		// Fetch + compile chunk loading for webassembly
+/******/ 		__webpack_require__.f.wasm = function(chunkId, promises) {
+/******/ 		
+/******/ 			var wasmModules = wasmModuleMap[chunkId] || [];
+/******/ 		
+/******/ 			wasmModules.forEach(function(wasmModuleId, idx) {
+/******/ 				var installedWasmModuleData = installedWasmModules[wasmModuleId];
+/******/ 		
+/******/ 				// a Promise means "currently loading" or "already loaded".
+/******/ 				if(installedWasmModuleData)
+/******/ 					promises.push(installedWasmModuleData);
+/******/ 				else {
+/******/ 					var importObject = wasmImportObjects[wasmModuleId]();
+/******/ 					var req = fetch(__webpack_require__.p + "" + {"src_Bot_ts":{"../pkg/htmf_wasm_bg.wasm":"c5b685e8ddf3880da35f"}}[chunkId][wasmModuleId] + ".module.wasm");
+/******/ 					var promise;
+/******/ 					if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
+/******/ 						promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
+/******/ 							return WebAssembly.instantiate(items[0], items[1]);
+/******/ 						});
+/******/ 					} else if(typeof WebAssembly.instantiateStreaming === 'function') {
+/******/ 						promise = WebAssembly.instantiateStreaming(req, importObject);
+/******/ 					} else {
+/******/ 						var bytesPromise = req.then(function(x) { return x.arrayBuffer(); });
+/******/ 						promise = bytesPromise.then(function(bytes) {
+/******/ 							return WebAssembly.instantiate(bytes, importObject);
+/******/ 						});
+/******/ 					}
+/******/ 					promises.push(installedWasmModules[wasmModuleId] = promise.then(function(res) {
+/******/ 						return __webpack_require__.w[wasmModuleId] = (res.instance || res).exports;
+/******/ 					}));
+/******/ 				}
+/******/ 			});
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// module cache are used so entry inlining is disabled
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/bot.worker.ts");
+/******/ })()
+;
