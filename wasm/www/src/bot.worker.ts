91a9a3b37ef218aa94399cbd6faf35be4d373198
Jeff Hemphill <jhemphill@fb.com>
2021-01-19 11:34:10 -0800

    Statically import bot

--- wasm/www/src/bot.worker.ts
+++ wasm/www/src/bot.worker.ts
@@ -1,6 +1,7 @@
+import Bot from "./Bot";
+
 const ctx: Worker = self as any;
 
-const Bot = (await import("./Bot")).default;
 const bot = new Bot(ctx.postMessage);
 ctx.onmessage = (event: MessageEvent) => {
   bot.onMessage(event.data);
