ff6d37e0b6961b2935a15f16c0b5fb693965979f
Jeff Hemphill <jthemphill@gmail.com>
2023-07-30 17:12:06 -0700

    Updates

---
+++ assets/bot.worker-9aa7327b.js
@@ -0,0 +1,2 @@
+Promise.all([import("./Bot-4c7448cb.js"),import("./htmf_wasm-c825f185.js").then(async a=>await a.default())]).then(([a,s])=>{const e=new a.default(postMessage);onmessage=t=>{e.onMessage(t.data)},e.init()});
+//# sourceMappingURL=bot.worker-9aa7327b.js.map
--- assets/bot.worker-f96467a4.js
+++
@@ -1,2 +0,0 @@
-Promise.all([import("./Bot-e5db3e99.js"),import("./htmf_wasm-555526e4.js").then(async a=>await a.default())]).then(([a,s])=>{const e=new a.default(postMessage);onmessage=t=>{e.onMessage(t.data)},e.init()});
-//# sourceMappingURL=bot.worker-f96467a4.js.map
