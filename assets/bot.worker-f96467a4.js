02ae1d10933a3cb5266a4f99dd66322406d60cc6
Jeff Hemphill <jthemphill@gmail.com>
2023-07-30 17:05:04 -0700

    Updates

---
+++ assets/bot.worker-f96467a4.js
@@ -0,0 +1,2 @@
+Promise.all([import("./Bot-e5db3e99.js"),import("./htmf_wasm-555526e4.js").then(async a=>await a.default())]).then(([a,s])=>{const e=new a.default(postMessage);onmessage=t=>{e.onMessage(t.data)},e.init()});
+//# sourceMappingURL=bot.worker-f96467a4.js.map
