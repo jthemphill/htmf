18f2e2fe64a579cf8bd4b37ec907088a68c33f90
Jeff Hemphill <jthemphill@gmail.com>
2023-09-08 17:21:21 -0700

    Use COOP and COEP in local development

--- wasm/www/vite.config.ts
+++ wasm/www/vite.config.ts
@@ -1,7 +1,23 @@
 /// <reference types="vitest" />
 
-import { defineConfig, searchForWorkspaceRoot } from 'vite'
+import { defineConfig, searchForWorkspaceRoot, type PluginOption } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
+// Some JS features are disabled unless you use these HTTP headers to promise not to load third-party scripts.
+// This plugin only affects local development. In production, you need to configure your webserver with these HTTP headers.
+// https://web.dev/coop-coep/
+function setUpCrossOriginIsolation (): PluginOption {
+  return {
+    name: "crossOriginIsolation",
+    configureServer (server): void {
+      server.middlewares.use((_req, res, next) => {
+        res.setHeader("Cross-Origin-Opener-Policy", "same-origin")
+        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp")
+        next()
+      })
+    },
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +26,7 @@ export default defineConfig({
     target: 'esnext',
     sourcemap: true
   },
-  plugins: [react()],
+  plugins: [react(), setUpCrossOriginIsolation()],
   resolve: { dedupe: ["react", "react-dom"] },
   server: {
     fs: { allow: [searchForWorkspaceRoot(process.cwd()), '../pkg'] }
