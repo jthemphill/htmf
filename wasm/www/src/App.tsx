f938dcde91588a165c0897e5478106b7346a0d82
Jeff Hemphill <jthemphill@gmail.com>
2023-09-10 17:47:30 -0700

    Break App up into smaller components

--- wasm/www/src/App.tsx
+++ wasm/www/src/App.tsx
@@ -1,22 +1,15 @@
 import React from 'react'
-import { type WorkerRequest, type WorkerResponse } from './WorkerProtocol'
 
 import Board from './Board'
 import type GameState from './GameState'
 import type { MoveScores } from './MoveScores'
 import { BOT_PLAYER, HUMAN_PLAYER, NPLAYERS } from './constants'
+import { type WorkerRequest, type WorkerResponse } from './WorkerProtocol'
 
 interface ThinkingProgress {
   completed: number
   required: number
   totalTimeMs: number
-}
-
-function getMemoryUsageFormatter (): Intl.NumberFormat {
-  return new Intl.NumberFormat(
-    navigator.language,
-    { notation: 'compact', style: 'unit', unit: 'byte', unitDisplay: 'narrow' }
-  )
 }
 
 export interface AppWorker extends Worker {
@@ -134,17 +127,6 @@ export default function App ({ worker }: Props): React.JSX.Element {
     ? <p>"Invalid move!"</p>
     : undefined
 
-  const scoresBlock = []
-  for (let p = 0; p < NPLAYERS; ++p) {
-    const playerClass = p === HUMAN_PLAYER ? 'human' : 'bot'
-    const active = gameState?.activePlayer === p ? '(Active Player)' : undefined
-    scoresBlock.push(
-      <p key={'score_' + p.toString()}><span className={playerClass}>
-        Score: {gameState?.scores[p]} {active}
-      </span></p>
-    )
-  }
-
   let topMove
   if (moveScores?.player === BOT_PLAYER && moveScores !== undefined) {
     for (const score of moveScores.tally) {
@@ -154,19 +136,49 @@ export default function App ({ worker }: Props): React.JSX.Element {
     }
   }
 
-  let board
-  if (gameState !== undefined) {
-    board = <Board
-      gameState={gameState}
-      possibleMoves={possibleMoves ?? []}
-      chosenCell={chosenCell}
-      handleCellClick={handleCellClick}
-      topMove={topMove}
-    />
-  }
+  return (
+    <div className="app">
+      {
+        gameState !== undefined &&
+        <Board
+          gameState={gameState}
+          possibleMoves={possibleMoves ?? []}
+          chosenCell={chosenCell}
+          handleCellClick={handleCellClick}
+          topMove={topMove}
+        />
+      }
+      <div className="info-col">
+        <p>{gameState?.modeType}</p>
+        {invalidMoveBlock}
+        {gameState !== undefined && <ScoresBlock activePlayer={gameState.activePlayer} scores={gameState.scores} />}
+        {moveScores !== undefined && <WinChanceMeter moveScores={moveScores} />}
+        <p className="tree-size">Stored {treeSize.toLocaleString()} game states in memory</p>
+        <MemoryUsageBlock memoryUsage={memoryUsage} treeSize={treeSize} />
+        {thinkingProgress !== undefined && <ThinkingProgressBar thinkingProgress={thinkingProgress} />}
+      </div>
+    </div>
+  )
+}
 
-  let winChanceMeter
-  if (moveScores !== undefined) {
+const ScoresBlock = React.memo(
+  function ScoresBlock ({ activePlayer, scores }: { activePlayer?: number, scores: number[] }): React.JSX.Element {
+    const scoresBlock = []
+    for (let p = 0; p < NPLAYERS; ++p) {
+      const playerClass = p === HUMAN_PLAYER ? 'human' : 'bot'
+      const active = activePlayer === p ? '(Active Player)' : undefined
+      scoresBlock.push(
+        <p key={'score_' + p.toString()}><span className={playerClass}>
+          Score: {scores[p]} {active}
+        </span></p>
+      )
+    }
+    return <div>{scoresBlock}</div>
+  }
+)
+
+const WinChanceMeter = React.memo(
+  function WinChanceMeter ({ moveScores }: { moveScores: MoveScores }): React.JSX.Element {
     let totalVisits = 0
     let totalRewards = 0
     for (const mov of moveScores.tally) {
@@ -179,43 +191,38 @@ export default function App ({ worker }: Props): React.JSX.Element {
       chance = 1 - chance
     }
 
-    winChanceMeter = (
+    return (
       <div className="win-chance">
         <meter id="win-chance-bar" min={0} max={1} low={0.49} high={0.5} optimum={1} value={chance} />
         <label htmlFor="win-chance-bar">Who's winning? (higher is better for you)</label>
       </div>
     )
   }
+)
 
-  let thinkingProgressBar
-  if (thinkingProgress !== undefined) {
+const MemoryUsageBlock = React.memo(
+  function MemoryUsageBlock ({ memoryUsage, treeSize }: { memoryUsage: number, treeSize: number }): React.JSX.Element {
+    const memoryUsageFormatter = new Intl.NumberFormat(
+      navigator.language,
+      { notation: 'compact', style: 'unit', unit: 'byte', unitDisplay: 'narrow' }
+    )
+    return (
+      <div className="memory-usage-block">
+        <p className="memory-usage">Using {memoryUsageFormatter.format(memoryUsage)}</p>
+        <p className="memory-ratio">Ratio is {memoryUsageFormatter.format(memoryUsage / treeSize)} per state</p>
+      </div>
+    )
+  }
+)
+
+const ThinkingProgressBar = React.memo(
+  function ThinkingProgressBar ({ thinkingProgress }: { thinkingProgress: ThinkingProgress }): React.JSX.Element {
     const playoutsPerSec = thinkingProgress.completed * 1000 / thinkingProgress.totalTimeMs
-    thinkingProgressBar = (
+    return (
       <div>
         <progress value={thinkingProgress.completed} max={thinkingProgress.required} />
         <p className="playout-counter">{`${playoutsPerSec.toFixed(0)} playouts/sec`}</p>
       </div>
     )
   }
-
-  const memoryUsageFormatter = React.useMemo(
-    getMemoryUsageFormatter,
-    [navigator.language]
-  )
-
-  return (
-    <div className="app">
-      {board}
-      <div className="info-col">
-        <p>{gameState?.modeType}</p>
-        {invalidMoveBlock}
-        <div>{scoresBlock}</div>
-        {winChanceMeter}
-        <p className="tree-size">Stored {treeSize.toLocaleString()} game states in memory</p>
-        <p className="memory-usage">Using {memoryUsageFormatter.format(memoryUsage)}</p>
-        <p className="memory-ratio">Ratio is {memoryUsageFormatter.format(memoryUsage / treeSize)} per state</p>
-        {thinkingProgressBar}
-      </div>
-    </div>
-  )
-}
+)
