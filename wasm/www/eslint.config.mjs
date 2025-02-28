7d5b95f94635b7a2fcb75ee682101e7ad454b2c9
Jeff Hemphill <jthemphill@gmail.com>
2025-02-28 12:46:11 -0800

    Update packages

---
+++ wasm/www/eslint.config.mjs
@@ -0,0 +1,53 @@
+import { fixupConfigRules } from "@eslint/compat";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      "**/__tests__",
+      "**/.eslintrc.cjs",
+      "**/eslint.config.mjs",
+      "**/dist",
+      "**/setup-vitest.ts",
+      "**/vite.config.ts",
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "plugin:@typescript-eslint/strict-type-checked",
+      "plugin:react-hooks/recommended",
+    ),
+  ),
+  {
+    plugins: {
+      "react-refresh": reactRefresh,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+];
